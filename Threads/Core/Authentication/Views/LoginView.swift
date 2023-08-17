dc08c28950f4c1d063695754c16a1b13ad3167c1
“rickysilitonga112” <rickysilitonga112@gmail.com>
2023-08-18 02:02:48 +0700

    Git init

---
+++ Threads/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Threads
+//
+//  Created by Ricky Silitonga on 18/08/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
