83dec21194035b3aa76e219bbf6263135a557370
“rickysilitonga112” <rickysilitonga112@gmail.com>
2023-08-18 01:58:46 +0700

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Ricky Silitonga on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
